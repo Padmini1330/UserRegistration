4ccf12b8ce06d9277be39df9b5fc2d2eab472a42
padmini v <padmini.v@perfios.com>
2021-09-07 22:25:20 +0530

    ADDED: method to validate the lastname

--- src/com/bridgelabz/userregistration/UserRegistrationRegEx.java
+++ src/com/bridgelabz/userregistration/UserRegistrationRegEx.java
@@ -7,24 +7,37 @@ import java.util.regex.Matcher;
 public class UserRegistrationRegEx 
 {
 
-	public static boolean firstNameValidator(String firstName) 
+	public void firstNameValidator(String firstName) 
 	{
 		Pattern pattern=Pattern.compile("[A-Z]{1}[a-zA-Z]{2,}");
 		Matcher matcher=pattern.matcher(firstName);
-		return matcher.matches();
+		if(matcher.matches())
+			System.out.println("Entered FirstName "+firstName+" is VALID");
+		else
+			System.out.println("Entered FirstName "+firstName+" is INVALID");
+	}
+	
+	public void lastNameValidator(String lastName) 
+	{
+		Pattern pattern=Pattern.compile("[A-Z]{1}[a-zA-Z]{2,}");
+		Matcher matcher=pattern.matcher(lastName);
+		if(matcher.matches())
+			System.out.println("Entered LastName "+lastName+" is VALID");
+		else
+			System.out.println("Entered LastName "+lastName+" is INVALID");
 	}
 
 	public static void main(String[] args) 
 	{
 		System.out.println("**Welcome to User Registration Validation**");
+		UserRegistrationRegEx userRegistration=new UserRegistrationRegEx();
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter valid FirstName:");
+		System.out.println("Enter valid FirstName and LastName:");
 		String firstName=scanner.next();
-		boolean isValid=firstNameValidator(firstName);
-		if(isValid)
-			System.out.println("Entered FirstName "+firstName+" is VALID");
-		else
-			System.out.println("Entered FirstName "+firstName+" is INVALID");
+		String lastName=scanner.next();
+		userRegistration.firstNameValidator(firstName);
+		userRegistration.lastNameValidator(lastName);
+			
 
 	}
 
