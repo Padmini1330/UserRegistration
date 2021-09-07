ae0b6793df178f7c7252b2bf9b40843aa839c573
padmini v <padmini.v@perfios.com>
2021-09-07 22:08:44 +0530

    ADDED: welcome message in main method

---
+++ src/com/bridgelabz/userregistration/UserRegistrationRegEx.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistrationRegEx 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**Welcome to User Registration Validation**");
+
+	}
+
+}
